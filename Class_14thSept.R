6cb796990b4762565781f172f7fbceb6f938dc7a
stachyris <klvinay.777@gmail.com>
2023-09-14 10:07:19 -0500

    14th sept class

---
+++ Class_14thSept.R
@@ -0,0 +1,22 @@
+#subsetting
+
+x = c(1,2,10,4,5)
+
+x[c(1,3)]
+
+y = c("A", "B", "C", "D")
+
+y[c(1,4)] #within the bracket it's the location and not the value. 
+
+x[-c(2,3)]
+x[c(-2, -3)]
+#above two works in the same way. You can add "-" in either side. 
+
+y[-c(1)] 
+
+
+#%% interesting operator. Modulus 
+# new operator ! 
+
+x[x %% 2 != 0]
+
